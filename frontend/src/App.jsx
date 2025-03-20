6a6e083604da76d05a3d15b4bc7d35b247b088a2
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2025-03-20 20:12:01 +0600

    implement daisyui in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
 function App() {
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      
+     <Navbar />
+
+     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
+     </Routes>
     </div>
   );
 }
